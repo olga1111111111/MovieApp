3d0b63c523c7f2c76875c041dd532e4fbf177afd
Olga <olgaabieva@ro.ru>
2021-12-23 22:34:55 +0300

    authentication

---
+++ lib/domain/api_client/api_client.dart
@@ -0,0 +1,78 @@
+import 'dart:io';
+import 'dart:convert';
+
+class ApiClient {
+  final _client = HttpClient();
+  static const _host = 'https://api.themoviedb.org/3';
+  static const _imageUrl = 'https://image.tmdb.org/t/p/w500';
+  static const _apiKey = '744d0b824522a8e452d1272a1a9b2d76';
+
+  Uri _makeUri(String path, [Map<String, dynamic>? parametrs]) {
+    final uri = Uri.parse('$_host$path');
+    if (parametrs != null) {
+      return uri.replace(queryParameters: parametrs);
+    } else {
+      return uri;
+    }
+  }
+
+  Future<String> makeToken() async {
+    final url = _makeUri(
+      '/authentication/token/new',
+      {'api_key': _apiKey},
+    );
+    final request = await _client.getUrl(url);
+    final response = await request.close();
+    final jsonStrings = await response.transform(utf8.decoder).toList();
+    final jsonString = jsonStrings.join();
+    final json = jsonDecode(jsonString) as Map<String, dynamic>;
+    final token = json['request_token'] as String;
+    return token;
+  }
+
+  Future<String> validateUser({
+    required String username,
+    required String password,
+    required String requestToken,
+  }) async {
+    final url = _makeUri(
+      '/authentication/token/validate_with_login',
+      {'api_key': _apiKey},
+    );
+    final parametrs = <String, dynamic>{
+      'username': username,
+      'password': password,
+      'request_token': requestToken,
+    };
+    final request = await _client.postUrl(url);
+    request.headers.contentType = ContentType.json;
+    request.write(jsonEncode(parametrs));
+    final response = await request.close();
+    final jsonStrings = await response.transform(utf8.decoder).toList();
+    final jsonString = jsonStrings.join();
+    final json = jsonDecode(jsonString) as Map<String, dynamic>;
+    final token = json['request_token'] as String;
+    return token;
+  }
+
+  Future<String> makeSession({
+    required String requestToken,
+  }) async {
+    final url = _makeUri(
+      '/authentication/session/new',
+      {'api_key': _apiKey},
+    );
+    final parametrs = <String, dynamic>{
+      'request_token': requestToken,
+    };
+    final request = await _client.postUrl(url);
+    request.headers.contentType = ContentType.json;
+    request.write(jsonEncode(parametrs));
+    final response = await request.close();
+    final jsonStrings = await response.transform(utf8.decoder).toList();
+    final jsonString = jsonStrings.join();
+    final json = jsonDecode(jsonString) as Map<String, dynamic>;
+    final sessionId = json['session_id'] as String;
+    return sessionId;
+  }
+}
