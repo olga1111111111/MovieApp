d307c546745fa9665adc333e8b556c33659381fe
Olga <olgaabieva@ro.ru>
2022-01-18 14:34:13 +0300

    поиск

--- lib/ui/widgets/movie_list/movie_list_model.dart
+++ lib/ui/widgets/movie_list/movie_list_model.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
 import 'package:themoviedb/domain/api_client/api_client.dart';
 import 'package:themoviedb/domain/entity/movie.dart';
+import 'package:themoviedb/domain/entity/popular_movie_response.dart';
 
 import 'package:themoviedb/ui/navigation/main_navigation.dart';
 
@@ -12,30 +15,49 @@ class MovieListModel extends ChangeNotifier {
   late int _totalPage;
   var _isLoadingInProgress = false;
   late int _currentPage;
+  String? _searchQuery;
   late DateFormat _dateFormat;
   String _locale = '';
+// запрос с задержкой таймера для отмены побуквенного запроса:
+  Timer? searchDebounce;
 
   String stringFromDate(DateTime? date) =>
       date != null ? _dateFormat.format(date) : "";
 
-  void setupLocale(BuildContext context) {
+  Future<void> setupLocale(BuildContext context) async {
     final locale = Localizations.localeOf(context).toLanguageTag();
     if (_locale == locale) return;
     _locale = locale;
-    _currentPage = 0;
-    _totalPage = 1;
     _dateFormat = DateFormat.yMMMd(locale);
-    _movies.clear();
-    _loadMovies();
-    print(locale);
+    await _resetList();
   }
 
-  Future<void> _loadMovies() async {
+  // при поисковом запросе:
+  Future<void> _resetList() async {
+    _currentPage = 0;
+    _totalPage = 1;
+    _movies.clear();
+    await _loadNextPage();
+  }
+
+  Future<PopularMovieResponse> _loadMovies(
+    int nextPage,
+    String locale,
+  ) async {
+    final query = _searchQuery;
+    if (query == null) {
+      return await _apiClient.popularMovie(nextPage, _locale);
+    } else {
+      return await _apiClient.searchMovie(nextPage, locale, query);
+    }
+  }
+
+  Future<void> _loadNextPage() async {
     if (_isLoadingInProgress || _currentPage >= _totalPage) return;
     _isLoadingInProgress = true;
     final nextPage = _currentPage + 1;
     try {
-      final movieResponse = await _apiClient.popularMovie(nextPage, _locale);
+      final movieResponse = await _loadMovies(nextPage, _locale);
       _movies.addAll(movieResponse.movie);
       _currentPage = movieResponse.page;
       _totalPage = movieResponse.totalPages;
@@ -52,8 +74,18 @@ class MovieListModel extends ChangeNotifier {
         .pushNamed(MainNavigationRouteNames.movieDetails, arguments: id);
   }
 
+  Future<void> searchMovie(String text) async {
+    searchDebounce?.cancel();
+    searchDebounce = Timer(const Duration(milliseconds: 300), () async {
+      final searchQuery = text.isNotEmpty ? text : null;
+      if (_searchQuery == searchQuery) return;
+      _searchQuery = searchQuery;
+      await _resetList();
+    });
+  }
+
   void showedMovieAtIndex(int index) {
     if (index < _movies.length - 1) return;
-    _loadMovies();
+    _loadNextPage();
   }
 }
--- lib/ui/widgets/movie_list/movie_list_widget.dart
+++ lib/ui/widgets/movie_list/movie_list_widget.dart
@@ -104,6 +104,7 @@ class MovieListWidget extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(10.0),
             child: TextField(
+              onChanged: model.searchMovie,
               decoration: InputDecoration(
                 labelText: 'Поиск',
                 filled: true,
